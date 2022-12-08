3bfdf9c75713cbccbe1f9277f0e707d73e4a5d2d
M4alik <mmalik.amara@gmail.com>
2022-12-08 14:30:36 +0100

    premiere etape

--- src/components/People.js
+++ src/components/People.js
@@ -2,7 +2,38 @@ import axios from "axios";
 import React, {useEffect, useState} from "react";
 
 export default function People(){
+    const [people, setPeople] = useState(null);
+    const [error, setError] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+
+    const url= `https://swapi.dev/api/people/4`;
+    useEffect(() => {axios.get(url)
+        .then(response => {
+            setPeople(response.data)
+            setLoaded(true);
+            console.log(response);
+        })
+
+        .catch(err => {
+            setError(err);
+            setLoaded(true);
+        })
+    },[url])
+    if(!loaded) {
+        return(<h1>En cours de chargement ...</h1>)
+    } else if(error) {
+        return (<h1>Erreur </h1>)
+    } else {
     return(
-     <h1>Hello world</h1>   
-    );    
+        <div>
+            <h1 className="text-center">Nom personnage : {people.name}</h1>
+            <p className="text-center">Taille : {people.height}</p>
+            <p className="text-center">Poids : {people.mass} </p>
+            <p className="text-center"> Couleur des yeux : {people.eye_color}</p>
+            <p className="text-center">Couleur des cheveux : {people.hair_color}</p>
+  
+        </div>
+
+    );
+    }    
 }
